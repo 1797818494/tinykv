7c9fd06eaaaf1c69e331ec24ad5d376a125b5b80
1797818494 <1797818494@qq.com>
2023-06-14 11:07:59 +0800

    finish p2

--- tinykv/mytest_parrel.sh
+++ tinykv/mytest_parrel.sh
@@ -7,13 +7,13 @@ do
     echo "ROUND $i to $((i+3)) PASSED";
     for ((j=0;j<4;j++));
     do
-        make project2c > ./out/out-$((i+j)).txt &
+        make project2c > ./out/out-$((i+j)).log &
     done
     wait
     rm -rf /tmp/*test-raftstore*
     for ((j=0;j<4;j++));
     do
-        if grep -q "FAIL" ./out/out-$((i+j)).txt; then
+        if grep -q "FAIL" ./out/out-$((i+j)).log; then
             echo "Error: Fail found in round $((i+j))"
             exit 1
         fi
--- tinykv/raft/raft.go
+++ tinykv/raft/raft.go
@@ -169,7 +169,7 @@ type Raft struct {
 
 // newRaft return a raft peer with the given config
 func newRaft(c *Config) *Raft {
-	log.SetLevel(log.LOG_LEVEL_ERROR)
+	// log.SetLevel(log.LOG_LEVEL_ERROR)
 	if err := c.validate(); err != nil {
 		panic(err.Error())
 	}
